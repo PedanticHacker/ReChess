a082e8f87cec50e703debd29d368b437417639be
Boštjan Mejak <bostjan.xperia@gmail.com>
2024-03-01 14:10:08 +0100

    Improve chess_clock.py

--- rechess/gui/widgets/chess_clock.py
+++ rechess/gui/widgets/chess_clock.py
@@ -7,41 +7,25 @@ from rechess import ClockStyle
 from rechess import get_config_value
 
 
-class CountdownTimer(QTimer):
-    """A 1-second countdown timer."""
-
-    def __init__(self) -> None:
-        super().__init__()
-
-        self.setInterval(1000)
-        self.setTimerType(Qt.TimerType.PreciseTimer)
-
-    def start_timer(self) -> None:
-        """Start the countdown timer."""
-        self.start()
-
-    def stop_timer(self) -> None:
-        """Stop the countdown timer."""
-        self.stop()
-
-
 class ChessClock(QLCDNumber):
-    """A digital chess clock with a 1-second countdown timer."""
+    """A clock with a 1-second countdown timer."""
 
     def __init__(self, clock_style: ClockStyle) -> None:
         super().__init__()
 
         self.setStyleSheet(clock_style)
         self.setFixedSize(QSize(200, 50))
-        self.setSegmentStyle(self.SegmentStyle.Flat)
+        self.setSegmentStyle(QLCDNumber.SegmentStyle.Flat)
 
-        self.countdown_timer: CountdownTimer = CountdownTimer()
-        self.countdown_timer.timeout.connect(self.update_time)
+        self._countdown_timer: QTimer = QTimer(self)
+        self._countdown_timer.setInterval(1000)
+        self._countdown_timer.setTimerType(Qt.TimerType.PreciseTimer)
+        self._countdown_timer.timeout.connect(self.update_time)
 
         self.reset()
 
     def reset(self) -> None:
-        """Reset the chess clock's time to values from the settings."""
+        """Reset the clock's time to values from the settings."""
         seconds: int = get_config_value("clock", "time")
         increment: int = get_config_value("clock", "increment")
         self.time: int = seconds + increment
@@ -49,13 +33,13 @@ class ChessClock(QLCDNumber):
         self.show_time()
 
     def show_time(self) -> None:
-        """Show the current chess clock's time."""
+        """Show the current clock's time."""
         time_text, time_text_length = self.format_time()
         self.setDigitCount(time_text_length)
         self.display(time_text)
 
     def format_time(self) -> tuple[str, int]:
-        """Format the chess clock's time as text and as text length."""
+        """Format the clock's time as text and as text length."""
         hours, remaining_time = divmod(self.time, 3600)
         minutes, seconds = divmod(remaining_time, 60)
 
@@ -67,12 +51,20 @@ class ChessClock(QLCDNumber):
         text_length: int = len(text)
         return text, text_length
 
+    def start_timer(self) -> None:
+        """Start the countdown timer."""
+        self._countdown_timer.start()
+
+    def stop_timer(self) -> None:
+        """Stop the countdown timer."""
+        self._countdown_timer.stop()
+
     @Slot()
     def update_time(self) -> None:
-        """Subtract 1 second from the chess clock's time."""
+        """Subtract 1 second from the clock's time."""
         if self.time > 0:
             self.time -= 1
             self.show_time()
         else:
-            self.countdown_timer.stop_timer()
+            self._countdown_timer.stop()
             # self.notify_player_lost()
