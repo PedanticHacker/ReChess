4f946b86e5d2c7328f97d62bd8771885060154e6
Boštjan Mejak <bostjan.xperia@gmail.com>
2024-03-02 19:03:19 +0100

    Improve settings_dialog.py yet again

--- rechess/gui/dialogs/settings_dialog.py
+++ rechess/gui/dialogs/settings_dialog.py
@@ -62,29 +62,34 @@ class SettingsDialog(QDialog):
 
         self.engine_white_option: QRadioButton = QRadioButton()
         self.engine_white_option.setText("White")
+        self.engine_white_option.setChecked(
+            not self.engine_black_option.isChecked()
+        )
 
-        self.pondering_option: QCheckBox = QCheckBox()
-        self.pondering_option.setText("Pondering")
-        self.pondering_option.setChecked(is_engine_pondering)
+        self.engine_pondering_option: QCheckBox = QCheckBox()
+        self.engine_pondering_option.setText("Pondering")
+        self.engine_pondering_option.setChecked(is_engine_pondering)
 
-        self.time_option: QComboBox = QComboBox()
-        self.time_option.addItem("1 minute", 60)
-        self.time_option.addItem("3 minutes", 180)
-        self.time_option.addItem("5 minutes", 300)
-        self.time_option.addItem("10 minutes", 600)
-        self.time_option.addItem("20 minutes", 1200)
-        self.time_option.addItem("30 minutes", 1800)
-        self.time_option.addItem("1 hour", 3600)
-        self.time_option.addItem("2 hours", 7200)
-        self.time_option.setCurrentIndex(self.time_option.findData(clock_time))
+        self.clock_time_option: QComboBox = QComboBox()
+        self.clock_time_option.addItem("1 minute", 60)
+        self.clock_time_option.addItem("3 minutes", 180)
+        self.clock_time_option.addItem("5 minutes", 300)
+        self.clock_time_option.addItem("10 minutes", 600)
+        self.clock_time_option.addItem("20 minutes", 1200)
+        self.clock_time_option.addItem("30 minutes", 1800)
+        self.clock_time_option.addItem("1 hour", 3600)
+        self.clock_time_option.addItem("2 hours", 7200)
+        self.clock_time_option.setCurrentIndex(
+            self.clock_time_option.findData(clock_time),
+        )
 
-        self.increment_option: QComboBox = QComboBox()
-        self.increment_option.addItem("0 seconds", 0)
-        self.increment_option.addItem("6 seconds", 6)
-        self.increment_option.addItem("12 seconds", 12)
-        self.increment_option.addItem("30 seconds", 30)
-        self.increment_option.setCurrentIndex(
-            self.increment_option.findData(clock_increment),
+        self.clock_increment_option: QComboBox = QComboBox()
+        self.clock_increment_option.addItem("0 seconds", 0)
+        self.clock_increment_option.addItem("6 seconds", 6)
+        self.clock_increment_option.addItem("12 seconds", 12)
+        self.clock_increment_option.addItem("30 seconds", 30)
+        self.clock_increment_option.setCurrentIndex(
+            self.clock_increment_option.findData(clock_increment),
         )
 
     def set_personal_layout(self) -> None:
@@ -92,12 +97,12 @@ class SettingsDialog(QDialog):
         engine_layout: QVBoxLayout = QVBoxLayout()
         engine_layout.addWidget(self.engine_black_option)
         engine_layout.addWidget(self.engine_white_option)
-        engine_layout.addWidget(self.pondering_option)
+        engine_layout.addWidget(self.engine_pondering_option)
         self.engine_group.setLayout(engine_layout)
 
         time_control_layout: QHBoxLayout = QHBoxLayout()
-        time_control_layout.addWidget(self.time_option)
-        time_control_layout.addWidget(self.increment_option)
+        time_control_layout.addWidget(self.clock_time_option)
+        time_control_layout.addWidget(self.clock_increment_option)
         self.time_control_group.setLayout(time_control_layout)
 
         personal_layout: QVBoxLayout = QVBoxLayout()
@@ -108,27 +113,27 @@ class SettingsDialog(QDialog):
 
     def connect_events_with_handlers(self) -> None:
         """Connect various events with specific handlers."""
-        self.accepted.connect(self.save_settings)
+        self.accepted.connect(self.on_save_settings)
         self.button_box.accepted.connect(self.accept)
         self.button_box.rejected.connect(self.reject)
 
     @Slot()
-    def save_settings(self) -> None:
+    def on_save_settings(self) -> None:
         """Save the changed settings."""
-        is_engine_white: bool = self.engine_black_option.isChecked()
-        is_engine_pondering: bool = self.pondering_option.isChecked()
+        is_engine_black: bool = self.engine_black_option.isChecked()
+        is_engine_pondering: bool = self.engine_pondering_option.isChecked()
 
-        clock_time: int = self.time_option.currentData()
-        clock_increment: int = self.increment_option.currentData()
+        clock_time: int = self.clock_time_option.currentData()
+        clock_increment: int = self.clock_increment_option.currentData()
 
         config_contents = {
-            "engine": {
-                "black": is_engine_white,
-                "pondering": is_engine_pondering,
-            },
             "clock": {
                 "time": clock_time,
                 "increment": clock_increment,
+            },
+            "engine": {
+                "black": is_engine_black,
+                "pondering": is_engine_pondering,
             },
         }
 
