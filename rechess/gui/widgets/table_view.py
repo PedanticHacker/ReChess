e5eacef1633ecc24930700d4511ee74f94580d2e
Boštjan Mejak <bostjan.xperia@gmail.com>
2024-03-17 21:00:04 +0100

    Minor tweak

--- rechess/gui/widgets/table_view.py
+++ rechess/gui/widgets/table_view.py
@@ -7,11 +7,6 @@ from PySide6.QtCore import (
 )
 
 
-CLEAR_AND_SELECT: QItemSelectionModel.SelectionFlag = (
-    QItemSelectionModel.SelectionFlag.ClearAndSelect
-)
-
-
 class TableView(QTableView):
     """A view for showing notation items in a 2-column table."""
 
@@ -19,9 +14,6 @@ class TableView(QTableView):
         super().__init__()
 
         self.setModel(table_model)
-
-        self._table_model: QAbstractTableModel = table_model
-        self._selection_model: QItemSelectionModel = self.selectionModel()
 
         self.configure_view()
         self.connect_events_with_handlers()
@@ -38,40 +30,50 @@ class TableView(QTableView):
 
     def connect_events_with_handlers(self) -> None:
         """Connect various events with specific handlers."""
-        self._table_model.layoutChanged.connect(self.scrollToBottom)
-        self._selection_model.selectionChanged.connect(self.on_selection_changed)
+        self.model().layoutChanged.connect(self.scrollToBottom)
+        self.selectionModel().selectionChanged.connect(self.on_selection_changed)
 
     def select_previous_item(self) -> None:
         """Select the previous notation item in the table."""
-        if not self._selection_model.hasSelection():
-            last_row = self._table_model.rowCount() - 1
-            last_column = self._table_model.columnCount() - 1
-            last_index = self._table_model.index(last_row, last_column)
-            self._selection_model.setCurrentIndex(last_index, CLEAR_AND_SELECT)
+        if not self.selectionModel().hasSelection():
+            last_row = self.model().rowCount() - 1
+            last_column = self.model().columnCount() - 1
+            last_index = self.model().index(last_row, last_column)
+            self.selectionModel().setCurrentIndex(
+                last_index,
+                QItemSelectionModel.SelectionFlag.Select,
+            )
+            return None
 
-        current_index = self._selection_model.currentIndex()
+        current_index = self.selectionModel().currentIndex()
         previous_row = current_index.row() - (current_index.column() == 0)
         previous_column = 0 if current_index.column() == 1 else 1
 
         if previous_row >= 0:
-            previous_index = self._table_model.index(previous_row, previous_column)
-            self._selection_model.setCurrentIndex(previous_index, CLEAR_AND_SELECT)
+            previous_index = self.model().index(previous_row, previous_column)
+            self.selectionModel().setCurrentIndex(
+                previous_index,
+                QItemSelectionModel.SelectionFlag.ClearAndSelect,
+            )
 
     def select_next_item(self) -> None:
         """Select the next notation item in the table."""
-        current_index = self._selection_model.currentIndex()
+        current_index = self.selectionModel().currentIndex()
         next_row = current_index.row() + (current_index.column() == 1)
         next_column = (current_index.column() + 1) % 2
 
-        if next_row < self._table_model.rowCount():
-            next_index = self._table_model.index(next_row, next_column)
-            self._selection_model.setCurrentIndex(next_index, CLEAR_AND_SELECT)
+        if next_row < self.model().rowCount():
+            next_index = self.model().index(next_row, next_column)
+            self.selectionModel().setCurrentIndex(
+                next_index,
+                QItemSelectionModel.SelectionFlag.ClearAndSelect,
+            )
 
     @Slot()
     def on_selection_changed(self) -> None:
         """Show the position when a notation item has been selected."""
-        current_index = self._selection_model.currentIndex()
+        current_index = self.selectionModel().currentIndex()
 
         if current_index.isValid():
-            item_data = self._table_model.data(current_index)
+            item_data = self.model().data(current_index)
             print(f"The data of the item is: {item_data}")
