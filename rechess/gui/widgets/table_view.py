5cf8dbdd8d51c4592102cc84f69b29119e24313f
Boštjan Mejak <bostjan.xperia@gmail.com>
2024-04-10 15:15:23 +0200

    dfygbysfh

--- rechess/gui/widgets/table_view.py
+++ rechess/gui/widgets/table_view.py
@@ -42,29 +42,27 @@ class TableView(QTableView):
 
     def select_prelast_item(self) -> None:
         """Select the notation item before the last."""
-        last_row = self.model().rowCount() - 1
-        second_column_index = self.model().index(last_row, 1)
-        second_column_data = self.model().data(second_column_index)
-        row = last_row if second_column_data else last_row - 1
-        column = 0 if second_column_data else 1
-        prelast_item_index = self.model().index(row, column)
+        last_row: int = self.model().rowCount() - 1
+        second_column_index: QModelIndex = self.model().index(last_row, 1)
+        second_column_data: str | None = self.model().data(second_column_index)
+        row: int = last_row if second_column_data else last_row - 1
+        column: int = 0 if second_column_data else 1
+        prelast_item_index: QModelIndex = self.model().index(row, column)
         self.selectionModel().setCurrentIndex(prelast_item_index, SELECT)
 
     def select_previous_item(self) -> None:
         """Select the previous notation item."""
-        if not self.has_selection() and self.linear_index != -1:
+        if not self.has_selection():
             self.select_prelast_item()
             return
 
-        previous_index = self.get_previous_index()
-
-        if previous_index.isValid():
-            self.selectionModel().setCurrentIndex(previous_index, SELECT)
+        previous_index: QModelIndex = self.get_previous_index()
+        self.selectionModel().setCurrentIndex(previous_index, SELECT)
 
     def select_next_item(self) -> None:
         """Select the next notation item."""
         if not self.has_selection():
-            first_index = self.model().index(0, 0)
+            first_index: QModelIndex = self.model().index(0, 0)
             self.selectionModel().setCurrentIndex(first_index, SELECT)
             return
 
@@ -74,20 +72,16 @@ class TableView(QTableView):
             self.selectionModel().setCurrentIndex(next_index, SELECT)
 
     def get_previous_index(self) -> QModelIndex:
-        """Get the QModelIndex of the previous item, if valid."""
-        if self.linear_index < 0:
-            return QModelIndex()
-
+        """Get the QModelIndex of the previous item."""
         previous_row, previous_column = divmod(self.linear_index - 1, 2)
         return self.model().index(previous_row, previous_column)
 
     def get_next_index(self) -> QModelIndex:
-        """Get the QModelIndex of the next item, if valid."""
+        """Get the QModelIndex of the next item."""
         next_row, next_column = divmod(self.linear_index + 1, 2)
 
         if next_row >= self.model().rowCount():
             return QModelIndex()
-
         return self.model().index(next_row, next_column)
 
     def has_selection(self) -> bool:
@@ -97,17 +91,13 @@ class TableView(QTableView):
     @property
     def linear_index(self) -> int:
         """Get a linear index of the currently selected item."""
-        current_index = self.selectionModel().currentIndex()
+        current_index: QModelIndex = self.selectionModel().currentIndex()
 
-        if not current_index.isValid():
-            return -1
-
-        return 2 * current_index.row() + current_index.column()
+        if current_index.isValid():
+            return 2 * current_index.row() + current_index.column()
+        return -1
 
     @Slot()
     def on_selection_changed(self) -> None:
         """Emit the selected notation item's linear index."""
-        if self.has_selection():
-            self.index_selected.emit(self.linear_index)
-        else:
-            self.index_selected.emit(-1)
+        self.index_selected.emit(self.linear_index)
