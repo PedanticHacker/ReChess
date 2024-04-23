06a8a6665f121ab69160322c0c0a17e3e6c18e1c
Boštjan Mejak <bostjan.xperia@gmail.com>
2024-04-23 20:24:29 +0200

    New improvements

--- rechess/gui/widgets/table_view.py
+++ rechess/gui/widgets/table_view.py
@@ -3,6 +3,7 @@ from PySide6.QtCore import (
     Slot,
     Signal,
     QModelIndex,
+    QAbstractItemModel,
     QAbstractTableModel,
     QItemSelectionModel,
 )
@@ -37,13 +38,15 @@ class TableView(QTableView):
         self.selectionModel().selectionChanged.connect(self.on_selection_changed)
 
     def select_prelast_item(self) -> None:
-        """Select the notation item before the last one."""
-        last_row: int = self.model().rowCount() - 1
-        second_column_index: QModelIndex = self.model().index(last_row, 1)
-        has_second_column_data: bool = self.has_data(second_column_index)
-        row: int = last_row if has_second_column_data else last_row - 1
-        column: int = 0 if has_second_column_data else 1
-        prelast_item_index: QModelIndex = self.model().index(row, column)
+        """Select an item before the last."""
+        model: QAbstractItemModel = self.model()
+        last_row: int = model.rowCount() - 1
+
+        if self.has_data(model.index(last_row, 1)):
+            prelast_item_index: QModelIndex = model.index(last_row, 1)
+        else:
+            prelast_item_index = model.index(last_row, 0)
+
         self.select(prelast_item_index)
 
     def select_previous_item(self) -> None:
@@ -52,29 +55,26 @@ class TableView(QTableView):
             self.select_prelast_item()
             return
 
-        previous_index: QModelIndex = self.get_previous_index()
-        self.select(previous_index)
+        self.select(self.previous_index())
 
     def select_next_item(self) -> None:
         """Select the next notation item by selecting its index."""
         if not self.has_selection():
-            first_index: QModelIndex = self.get_first_index()
-            self.select(first_index)
+            self.select(self.first_index())
             return
 
-        next_index: QModelIndex = self.get_next_index()
-        self.select(next_index)
+        self.select(self.next_index())
 
-    def get_first_index(self) -> QModelIndex:
+    def first_index(self) -> QModelIndex:
         """Get an index of the first notation item."""
         return self.model().index(0, 0)
 
-    def get_previous_index(self) -> QModelIndex:
+    def previous_index(self) -> QModelIndex:
         """Get an index of the previous item."""
         previous_row, previous_column = divmod(self.linear_index - 1, 2)
         return self.model().index(previous_row, previous_column)
 
-    def get_next_index(self) -> QModelIndex:
+    def next_index(self) -> QModelIndex:
         """Get an index of the next item."""
         next_row, next_column = divmod(self.linear_index + 1, 2)
         return self.model().index(next_row, next_column)
@@ -87,16 +87,16 @@ class TableView(QTableView):
         )
 
     def has_data(self, index: QModelIndex) -> bool:
-        """Check whether a notation item with the `index` has data."""
+        """Check whether an item with the `index` has data."""
         return self.model().data(index)
 
     def has_selection(self) -> bool:
-        """Check whether any notation item is currently selected."""
+        """Check whether any item is currently selected."""
         return self.selectionModel().hasSelection()
 
     @property
     def linear_index(self) -> int:
-        """Get the linear index of a selected notation item."""
+        """Return a linear index of the currently selected item."""
         current_index: QModelIndex = self.selectionModel().currentIndex()
 
         if current_index.isValid():
@@ -106,6 +106,6 @@ class TableView(QTableView):
 
     @Slot(int)
     def on_selection_changed(self) -> None:
-        """Emit the row index of a selected notation item."""
+        """Emit the index of the currently selected row."""
         row_index: int = self.selectionModel().currentIndex().row()
         self.item_selected.emit(row_index)
