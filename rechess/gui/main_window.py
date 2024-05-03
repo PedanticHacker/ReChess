5fb6bfa2d15f72d45faa5b0fb3508b68bfc09ef7
Boštjan Mejak <bostjan.xperia@gmail.com>
2024-05-03 10:34:50 +0200

    FIX!!!

--- rechess/gui/main_window.py
+++ rechess/gui/main_window.py
@@ -506,7 +506,7 @@ class MainWindow(QMainWindow):
             if self._table_view.has_selection():
                 self._table_view.clearFocus()
                 self._table_view.clearSelection()
-                self._game.delete_data_after(self._table_view.linear_index)
+                self._game.delete_data_after(self._table_view.sequential_index)
 
             self._game.push(move)
             self.refresh_ui()
