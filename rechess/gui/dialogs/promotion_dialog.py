3550e660cc6b3a232d2ed90ac81e17544aa63d80
Boštjan Mejak <bostjan.xperia@gmail.com>
2024-06-02 21:25:38 +0200

    Reorder import

--- rechess/gui/dialogs/promotion_dialog.py
+++ rechess/gui/dialogs/promotion_dialog.py
@@ -1,4 +1,4 @@
-from chess import BISHOP, KNIGHT, QUEEN, ROOK, WHITE, Color, PieceType
+from chess import BISHOP, Color, KNIGHT, PieceType, QUEEN, ROOK, WHITE
 from PySide6.QtWidgets import QDialog, QDialogButtonBox, QHBoxLayout
 
 from rechess.utils import create_button, get_svg_icon
