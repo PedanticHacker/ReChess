6834b01552c398a0c46c0b6b113b736df470fbd2
Boštjan Mejak <bostjan.xperia@gmail.com>
2024-07-02 21:02:10 +0200

    Fixed some screwup

--- rechess/gui/widgets/table_view.py
+++ rechess/gui/widgets/table_view.py
@@ -11,8 +11,6 @@ from PySide6.QtWidgets import QAbstractItemView, QHeaderView, QTableView
 class TableView(QTableView):
     """A view for displaying notation items in a 2-column table."""
 
-    item_selected: Signal = Signal(int)
-
     def __init__(self, table_model: QAbstractTableModel) -> None:
         super().__init__()
 
@@ -27,7 +25,6 @@ class TableView(QTableView):
         self.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
 
         self.model().layoutChanged.connect(self.scrollToBottom)
-        self.selectionModel().currentChanged.connect(self.on_current_changed)
 
     def select_last_item(self) -> None:
         """Select the last notation item."""
@@ -44,24 +41,18 @@ class TableView(QTableView):
         """Select the next notation item."""
         self.select_model_index(self.get_next_model_index())
 
-    def select_first_item(self) -> None:
-        """Select the first notation item."""
-        first_model_index = self.model().index(0, 0)
-        self.select_model_index(first_model_index)
-
     def get_previous_model_index(self) -> QModelIndex:
         """Get a model index of the previous notation item."""
-        if self.current_ply_index > -1:
-            previous_row, previous_column = divmod(self.current_ply_index - 1, 2)
-            return self.model().index(previous_row, previous_column)
+        previous_row, previous_column = divmod(self.ply_index - 1, 2)
+        return self.model().index(previous_row, previous_column)
 
     def get_next_model_index(self) -> QModelIndex:
         """Get a model index of the next notation item."""
-        if self.current_ply_index == -1:
-            return self.first_index()
-
-        next_row, next_column = divmod(self.current_ply_index + 1, 2)
-        return self.model().index(next_row, next_column)
+        if self.ply_index < 0:
+            return self.model().index(0, 0)
+        else:
+            next_row, next_column = divmod(self.ply_index + 1, 2)
+            return self.model().index(next_row, next_column)
 
     def select_model_index(self, model_index: QModelIndex) -> None:
         """Select a notation item with the `model_index`."""
