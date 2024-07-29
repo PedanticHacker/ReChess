ab6bdc3384d858ff77c6de7f44cbb83bb51c1cc6
Boštjan Mejak <bostjan.xperia@gmail.com>
2024-07-29 20:29:43 +0200

    Yet another improvement

--- rechess/types/enums.py
+++ rechess/types/enums.py
@@ -2,7 +2,7 @@ from enum import StrEnum
 
 
 class ClockColor(StrEnum):
-    """CSS colors enum for Black's and White's chess clock widget."""
+    """CSS color enum for Black's and White's chess clock widget."""
 
     Black: str = "color: white; background-color: black;"
     White: str = "color: black; background-color: white;"
