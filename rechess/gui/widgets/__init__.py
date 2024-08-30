19c4601fde95df84ec1ef0d6f0a65d72bddcf6fa
Boštjan Mejak <bostjan.xperia@gmail.com>
2024-08-30 16:03:39 +0200

    afvaervev

--- rechess/gui/widgets/__init__.py
+++ rechess/gui/widgets/__init__.py
@@ -1,7 +1,6 @@
 from .chess_clock import ChessClockWidget
 from .evaluation_bar import EvaluationBarWidget
 from .fen_editor import FenEditorWidget
-from .main_window import MainWindow
 from .svg_board import SvgBoardWidget
 
 __all__: list[str] = [
