a32592d0c8593434b313be7bf437b0e2546e7c1f
Boštjan Mejak <bostjan.xperia@gmail.com>
2024-10-15 20:10:10 +0200

    Truncate analysis variation text to 15 characters

--- rechess/engine/uci.py
+++ rechess/engine/uci.py
@@ -62,7 +62,7 @@ class UciEngine(QObject):
                     break
 
                 if "pv" in info:
-                    pv: list[Move] = info["pv"]
+                    pv: list[Move] = info["pv"][0:15]
 
                     best_move: Move = pv[0]
                     variation: str = self._game.board.variation_san(pv)
