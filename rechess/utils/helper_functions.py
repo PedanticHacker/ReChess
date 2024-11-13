16366f93262217da6114ad2b9d5c481f118b8aa4
Boštjan Mejak <bostjan.xperia@gmail.com>
2024-11-13 21:47:41 +0100

    dsgbstbsetb

--- rechess/utils/helper_functions.py
+++ rechess/utils/helper_functions.py
@@ -11,7 +11,7 @@ from PySide6.QtGui import QAction, QColor, QIcon, QPixmap
 from PySide6.QtWidgets import QApplication, QPushButton
 
 
-PATH_TO_SETTINGS: str = "rechess/settings.json"
+PATH_TO_SETTINGS_FILE: str = "rechess/settings.json"
 
 
 BoardSection: TypeAlias = Literal["board"]
@@ -28,6 +28,43 @@ SettingValue: TypeAlias = bool | float
 SettingsDict: TypeAlias = dict[str, dict[str, SettingValue]]
 
 
+def platform_name() -> str:
+    """Return platform name in lowercase."""
+    name: str = platform.system().lower()
+    return "macos" if name == "darwin" else name
+
+
+def _stockfish_filename() -> str:
+    """Return platform-specific filename of Stockfish chess engine."""
+    return "stockfish.exe" if platform_name() == "windows" else "stockfish"
+
+
+def path_to_stockfish() -> str:
+    """Return path to executable file of Stockfish 17 chess engine."""
+    return (
+        f"rechess/assets/engines/stockfish-17/{platform_name()}"
+        f"/{_stockfish_filename()}"
+    )
+
+
+def make_executable(path_to_file: str) -> None:
+    """Make file at `path_to_file` be executable."""
+    os.chmod(path_to_file, os.stat(path_to_file).st_mode | stat.S_IXUSR)
+
+
+def delete_quarantine_attribute(path_to_file: str) -> None:
+    """Delete quarantine attribute for file at `path_to_file` on macOS."""
+    if platform_name() == "macos":
+        file_attributes: str = subprocess.run(
+            ["xattr", "-l", path_to_file],
+            capture_output=True,
+            text=True,
+        ).stdout
+
+        if "com.apple.quarantine" in file_attributes:
+            subprocess.run(["xattr", "-d", "com.apple.quarantine", path_to_file])
+
+
 def _optimal_cpu_threads() -> int:
     """Return all CPU threads, but reserve 1 for other CPU tasks."""
     cpu_threads: int | None = cpu_count()
@@ -41,45 +78,69 @@ def _optimal_hash_size() -> int:
     return available_ram // SEVENTY_PERCENT
 
 
+def engine_configuration() -> dict[str, int]:
+    """Return optimal configuration for UCI chess engine."""
+    return {"Hash": _optimal_hash_size(), "Threads": _optimal_cpu_threads()}
+
+
 def _settings() -> SettingsDict:
-    """Return all settings from file."""
-    with open(PATH_TO_SETTINGS) as file:
-        return json.load(file)
-
-
-def _stockfish_filename() -> str:
-    """Return platform-specific filename of Stockfish chess engine."""
-    return "stockfish.exe" if platform_name() == "windows" else "stockfish"
+    """Return all settings from settings file."""
+    with open(PATH_TO_SETTINGS_FILE) as settings_file:
+        return json.load(settings_file)
 
 
 @overload
-def setting_value(section: BoardSection, key: BoardKey) -> bool: ...
+def setting_value(
+    section: BoardSection,
+    key: BoardKey,
+) -> bool: ...
 
 
 @overload
-def setting_value(section: ClockSection, key: ClockKey) -> float: ...
+def setting_value(
+    section: ClockSection,
+    key: ClockKey,
+) -> float: ...
 
 
 @overload
-def setting_value(section: EngineSection, key: EngineKey) -> bool: ...
+def setting_value(
+    section: EngineSection,
+    key: EngineKey,
+) -> bool: ...
 
 
-def setting_value(section: SettingSection, key: SettingKey) -> SettingValue:
+def setting_value(
+    section: SettingSection,
+    key: SettingKey,
+) -> SettingValue:
     """Return value of `key` from `section`."""
     settings: SettingsDict = _settings()
     return settings[section][key]
 
 
 @overload
-def set_setting_value(section: BoardSection, key: BoardKey, value: bool) -> None: ...
+def set_setting_value(
+    section: BoardSection,
+    key: BoardKey,
+    value: bool,
+) -> None: ...
 
 
 @overload
-def set_setting_value(section: ClockSection, key: ClockKey, value: float) -> None: ...
+def set_setting_value(
+    section: ClockSection,
+    key: ClockKey,
+    value: float,
+) -> None: ...
 
 
 @overload
-def set_setting_value(section: EngineSection, key: EngineKey, value: bool) -> None: ...
+def set_setting_value(
+    section: EngineSection,
+    key: EngineKey,
+    value: bool,
+) -> None: ...
 
 
 def set_setting_value(
@@ -88,12 +149,36 @@ def set_setting_value(
     value: SettingValue,
 ) -> None:
     """Set `value` to `key` for `section`."""
-    settings: SettingsDict = _settings()
-    settings[section][key] = value
+    settings_dict: SettingsDict = _settings()
+    settings_dict[section][key] = value
 
-    with open(PATH_TO_SETTINGS, mode="w", encoding="utf-8", newline="\n") as file:
-        json.dump(settings, file, indent=2)
-        file.write("\n")
+    with open(
+        PATH_TO_SETTINGS_FILE,
+        mode="w",
+        encoding="utf-8",
+        newline="\n",
+    ) as settings_file:
+        json.dump(settings_dict, settings_file, indent=2)
+        settings_file.write("\n")
+
+
+def find_opening(fen: str) -> tuple[str, str] | None:
+    """Return ECO code and opening name for `fen`."""
+    with open("rechess/openings.json", encoding="utf-8") as json_file:
+        openings = json.load(json_file)
+    return openings.get(fen)
+
+
+def initialize_app() -> QApplication:
+    """Initialize app with basic settings."""
+    app: QApplication = QApplication()
+    app.setApplicationDisplayName("ReChess")
+    app.setApplicationName("ReChess")
+    app.setApplicationVersion("1.0")
+    app.setDesktopFileName("ReChess")
+    app.setStyle("fusion")
+    app.setWindowIcon(svg_icon("logo"))
+    return app
 
 
 def create_action(
@@ -117,62 +202,6 @@ def create_button(icon: QIcon) -> QPushButton:
     button.setIcon(icon)
     button.setIconSize(QSize(56, 56))
     return button
-
-
-def delete_quarantine_attribute(path_to_file: str) -> None:
-    """Delete quarantine attribute for file at `path_to_file` on macOS."""
-    if platform_name() == "macos":
-        file_attributes: str = subprocess.run(
-            ["xattr", "-l", path_to_file],
-            capture_output=True,
-            text=True,
-        ).stdout
-
-        if "com.apple.quarantine" in file_attributes:
-            subprocess.run(["xattr", "-d", "com.apple.quarantine", path_to_file])
-
-
-def engine_configuration() -> dict[str, int]:
-    """Return optimal configuration for UCI chess engine."""
-    return {"Hash": _optimal_hash_size(), "Threads": _optimal_cpu_threads()}
-
-
-def find_opening(fen: str) -> tuple[str, str] | None:
-    """Return ECO code and opening name for `fen`."""
-    with open("rechess/openings.json", encoding="utf-8") as json_file:
-        openings = json.load(json_file)
-    return openings.get(fen)
-
-
-def initialize_app() -> QApplication:
-    """Initialize ReChess GUI app with basic settings."""
-    app: QApplication = QApplication()
-    app.setApplicationDisplayName("ReChess")
-    app.setApplicationName("ReChess")
-    app.setApplicationVersion("1.0")
-    app.setDesktopFileName("ReChess")
-    app.setStyle("fusion")
-    app.setWindowIcon(svg_icon("logo"))
-    return app
-
-
-def make_executable(path_to_file: str) -> None:
-    """Make file at `path_to_file` be executable."""
-    os.chmod(path_to_file, os.stat(path_to_file).st_mode | stat.S_IXUSR)
-
-
-def path_to_stockfish() -> str:
-    """Return path to executable file of Stockfish 17 chess engine."""
-    return (
-        f"rechess/assets/engines/stockfish-17/{platform_name()}"
-        f"/{_stockfish_filename()}"
-    )
-
-
-def platform_name() -> str:
-    """Return platform name in lowercase."""
-    name: str = platform.system().lower()
-    return "macos" if name == "darwin" else name
 
 
 def style_icon(color: str) -> QIcon:
