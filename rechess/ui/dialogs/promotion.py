8a8bad75c8a7daca92fae311a51ddfea30ad49b5
Boštjan Mejak <bostjan.xperia@gmail.com>
2024-12-03 20:48:28 +0100

    sgbsgbsgn

--- rechess/ui/dialogs/promotion.py
+++ rechess/ui/dialogs/promotion.py
@@ -5,7 +5,7 @@ from rechess.utils import create_button, svg_icon
 
 
 class PromotionDialog(QDialog):
-    """Dialog with buttons for selecting promotion piece type."""
+    """Dialog with buttons for selecting pawn promotion piece type."""
 
     def __init__(self, piece_color: Color) -> None:
         super().__init__()
@@ -23,7 +23,7 @@ class PromotionDialog(QDialog):
         self.setWindowTitle("Pawn Promotion")
 
     def create_buttons(self) -> None:
-        """Create buttons for each promotion piece type based on piece color."""
+        """Create buttons based on piece color showing piece type."""
         if self._piece_color == WHITE:
             self.queen_button = create_button(svg_icon("white-queen"))
             self.rook_button = create_button(svg_icon("white-rook"))
@@ -53,26 +53,26 @@ class PromotionDialog(QDialog):
         self.knight_button.clicked.connect(self.on_knight_button_clicked)
 
     def on_queen_button_clicked(self) -> None:
-        """Set promotion piece type to queen."""
+        """Set piece type to queen."""
         self._piece_type = QUEEN
         self.accept()
 
     def on_rook_button_clicked(self) -> None:
-        """Set promotion piece type to rook."""
+        """Set piece type to rook."""
         self._piece_type = ROOK
         self.accept()
 
     def on_bishop_button_clicked(self) -> None:
-        """Set promotion piece type to bishop."""
+        """Set piece type to bishop."""
         self._piece_type = BISHOP
         self.accept()
 
     def on_knight_button_clicked(self) -> None:
-        """Set promotion piece type to knight."""
+        """Set piece type to knight."""
         self._piece_type = KNIGHT
         self.accept()
 
     @property
     def piece_type(self) -> PieceType:
-        """Return selected promotion piece type."""
+        """Return selected piece type."""
         return self._piece_type
