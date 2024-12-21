9c427554821e238af5e82c4fe11f9a7d9bbfe7c9
Boštjan Mejak <bostjan.xperia@gmail.com>
2024-12-21 06:57:16 +0100

    djgjdgamgkdaksk

--- rechess/ui/enums.py
+++ rechess/ui/enums.py
@@ -4,5 +4,5 @@ from enum import StrEnum
 class ClockColor(StrEnum):
     """CSS color style enum for clocks of Black and White players."""
 
-    Black: str = "color: white; background-color: black;"
-    White: str = "color: black; background-color: white;"
+    Black = "color: white; background-color: black;"
+    White = "color: black; background-color: white;"
