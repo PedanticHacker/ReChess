7128800ce57bc65360a4bcb5ecc65f165f539106
Boštjan Mejak <bostjan.xperia@gmail.com>
2024-12-22 17:53:10 +0100

    fhsjgbowehibgosehbvwoivbwljhvbwlfhkbv

--- rechess/core/game.py
+++ rechess/core/game.py
@@ -41,24 +41,7 @@ class Game(QObject):
         self._arrows: list[Arrow] = []
         self._moves: list[str] = []
         self._positions: list[Board] = []
-
-        self._capture_sound_effect: QSoundEffect = QSoundEffect()
-        self._capture_sound_effect.setSource(CAPTURE_FILE_URL)
-
-        self._castling_sound_effect: QSoundEffect = QSoundEffect()
-        self._castling_sound_effect.setSource(CASTLING_FILE_URL)
-
-        self._check_sound_effect: QSoundEffect = QSoundEffect()
-        self._check_sound_effect.setSource(CHECK_FILE_URL)
-
-        self._game_over_sound_effect: QSoundEffect = QSoundEffect()
-        self._game_over_sound_effect.setSource(GAME_OVER_FILE_URL)
-
-        self._move_sound_effect: QSoundEffect = QSoundEffect()
-        self._move_sound_effect.setSource(MOVE_FILE_URL)
-
-        self._promotion_sound_effect: QSoundEffect = QSoundEffect()
-        self._promotion_sound_effect.setSource(PROMOTION_FILE_URL)
+        self._sound_effect: QSoundEffect = QSoundEffect()
 
         self.prepare_new_game()
 
@@ -157,17 +140,19 @@ class Game(QObject):
     def play_sound_effect(self, move: Move) -> None:
         """Play sound effect for `move`."""
         if self.is_over_after(move):
-            self._game_over_sound_effect.play()
+            self._sound_effect.setSource(GAME_OVER_FILE_URL)
         elif self.is_check(move):
-            self._check_sound_effect.play()
+            self._sound_effect.setSource(CHECK_FILE_URL)
         elif move.promotion:
-            self._promotion_sound_effect.play()
+            self._sound_effect.setSource(PROMOTION_FILE_URL)
         elif self._board.is_capture(move):
-            self._capture_sound_effect.play()
+            self._sound_effect.setSource(CAPTURE_FILE_URL)
         elif self._board.is_castling(move):
-            self._castling_sound_effect.play()
+            self._sound_effect.setSource(CASTLING_FILE_URL)
         else:
-            self._move_sound_effect.play()
+            self._sound_effect.setSource(MOVE_FILE_URL)
+
+        self._sound_effect.play()
 
     def set_root_position(self) -> None:
         """Reset pieces on board to initial position."""
